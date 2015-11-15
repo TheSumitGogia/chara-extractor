06e51b12a9c14cb661ccb6e658d64ebbc2d920d9
mzhang94 <mzhang94@mit.edu>
2015-11-14 21:53:22 -0500

    all books

--- data_collect.py
+++ data_collect.py
@@ -10,11 +10,16 @@ class Book:
         self.characters = {}
 
     def getCharactersFromWeb(self):
-        page = urllib2.urlopen(self.url)
+        try:
+            page = urllib2.urlopen(self.url)
+        except urllib2.HTTPError as e:
+            return
         soup = BeautifulSoup(page.read(), convertEntities=BeautifulSoup.HTML_ENTITIES)
         regex = re.compile('<!--\\n.*DisplayAds.*\\n.*-->')
         for div in soup.findAll("div", {"class" :"content_txt"}):
             name = div.get('id')
+            if name == None:
+                continue
             # get rid of ads and remove first line
             text = regex.sub("", div.text)
             text = text.split('\n')[1:]
@@ -35,6 +40,8 @@ class Book:
             self.characters[name] = text
 
     def writeToFile(self):
+        if len(self.characters) == 0:
+            return 
         directory = 'books/%s' % self.name
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -42,10 +49,19 @@ class Book:
             f = open('%s/%s' % (directory, name), 'w')
             f.write(self.characters[name])
 
+'''
+book = Book('elegantuniverse')
+book.getCharactersFromWeb()
+book.writeToFile()
+'''
+exclude = set(['elegantuniverse', 'earnest', 'thinair'])
 books = set([])
 for l in string.ascii_lowercase:
     url = "http://www.sparknotes.com/lit/index_%s.html" % l
-    page = urllib2.urlopen(url)
+    try:
+        page = urllib2.urlopen(url)
+    except urllib2.HTTPError as e:
+        continue 
     html = page.read()
     soup = BeautifulSoup(html)
 
@@ -53,9 +69,17 @@ for l in string.ascii_lowercase:
         url = link.get('href')
         if not url == None and url.startswith("http://www.sparknotes.com/lit/"):
             book = url[30:]
-            if book != "" and not book.startswith("index"):
+            if len(book) != 0 and book[len(book)-1] == '/':
+                book = book[:len(book)-1]
+            if book != "" and not book.startswith("index") and not book in books and not book in exclude:
                 print 'getting book %s' % book
                 books.add(book)
                 book = Book(book)
-                book.getCharactersFromWeb()
-                book.writeToFile()
+                try:
+                    book.getCharactersFromWeb()
+                    book.writeToFile()
+                except:
+                    print "ERROR getting book %s" % book.name
+                    exclude.add(book.name)
+
+print exclude
