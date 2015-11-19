b0fad10e53268f4ce2747695c6a1f06fa7f3609b
Sumit Gogia <summit@mit.edu>
2015-11-19 14:31:19 -0500

    Added batch processing of raw texts to CoreNLP script

--- parsing.py
+++ parsing.py
@@ -2,7 +2,7 @@ import os
 import argparse
 from subprocess import call
 
-def run_nlp(nlp_dir, book_dir):
+def run_spark_nlp(nlp_dir, book_dir, out_dir):
     all_books = os.listdir(book_dir)
     for dirpath in all_books:
         all_charas = os.listdir(book_dir + "/" + dirpath)
@@ -11,16 +11,33 @@ def run_nlp(nlp_dir, book_dir):
         chara_file = open("chara_file.txt", "w")
         chara_file.write("\n".join(all_charas))
         chara_file.close()
-        call(["bash", nlp_dir + "/corenlp.sh", "--annotators", "tokenize,ssplit,pos,lemma,ner,depparse,parse,dcoref", "-filelist", "chara_file.txt", "-outputDirectory", book_dir + "/" + dirpath])
+        call(["bash", nlp_dir + "/corenlp.sh", "--annotators", "tokenize,ssplit,pos,lemma,ner,depparse,parse,dcoref", "-filelist", "chara_file.txt", "-outputDirectory", out_dir + "/" + dirpath])
 
+def run_book_nlp(nlp_dir, book_dir, out_dir):
+    # get all books in book full text dir
+    all_books = os.listdir(book_dir)
+
+    # create book name index for batch CoreNLP processing
+    book_list = open(book_dir + '/book_list.txt', 'w')
+    book_list.write('\n'.join(all_books))
+    book_list.close()
+
+    call(["bash", nlp_dir + "/corenlp.sh", "--annotators", "tokenize,ssplit,pos,lemma,ner", "-filelist", book_dir + "/book_list.txt", "-outputDirector", out_dir])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Stanford NLP tools on Sparknotes data")
+    parser.add_argument('-t', '--type', nargs=1, type=int, required=True, help='Annotation filetype')
     parser.add_argument('-d', '--nlpdir', nargs=1, required=True, help='Stanford NLP root directory')
     parser.add_argument('-b', '--booksdir', nargs=1, required=True, help='Book character lists directory')
+    parser.add_argument('-o', '--outdir', nargs=1, required=True, help='Annotation output directory')
 
     args = vars(parser.parse_args())
+    filetype = args['type'][0]
     book_dir = args['booksdir'][0]
+    out_dir = args['outdir'][0]
     nlp_dir = args['nlpdir'][0]
 
-    run_nlp(nlp_dir, book_dir)
+    if filetype == 0:
+        run_spark_nlp(nlp_dir, book_dir, out_dir)
+    elif filetype == 1:
+        run_book_nlp(nlp_dir, book_dir, out_dir)
