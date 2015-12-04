2ef048215187741ead935d03d6c3e120b2e21e83
Tommy Zhang <trzhang@dhcp-18-189-87-202.dyn.mit.edu>
2015-12-04 02:51:55 -0500

    fixed comparison issue and added blacklist

--- crossref.py
+++ crossref.py
@@ -12,19 +12,29 @@ gutenberg = open('gutenberg/GUTINDEX.ALL.txt').read().lower()
 good_file = open('gutenberg/good.txt', 'w')
 bad_file = open('gutenberg/bad.txt', 'w')
 ugly_file = open('gutenberg/ugly.txt', 'w')
+blacklist_path = 'gutenberg/blacklist.txt'
+blacklist = set([])
+
+with open(blacklist_path) as f:
+  for book in f:
+    blacklist.add(book.strip())
 
 good, bad, ugly = 0, 0, 0
 for book in sparknotes:
+  if book in blacklist:
+    continue
   # Sparknotes fail check
   try:
     page = urllib2.urlopen(sparknotes_url % book)
   except urllib2.HTTPError:
+    blacklist.add(book)
     continue
   # Get title and author from sparknotes
   soup = BeautifulSoup(page.read())
   title = soup.findAll('h1', {'class': 'title padding-btm-0'})[0].text.encode('utf-8').lower()
   author = soup.findAll('h2', {'class': 'author'})[0].text.encode('utf-8').lower()
   if len(author) == 0:
+    blacklist.add(book)
     continue
   last_name = author.split(' ')[-1]
 
@@ -35,9 +45,11 @@ for book in sparknotes:
   soup = BeautifulSoup(page.read())
   tag = soup.findAll('div', {'class': '_gdf'})
   if len(tag) == 0:
+    blacklist.add(book)
     continue
   desc = soup.findAll('div', {'class': '_gdf'})[0].text.lower()
   if 'play' in desc or 'poem' in desc:
+    blacklist.add(book)
     continue
 
   # Author in gutenberg index
@@ -50,7 +62,7 @@ for book in sparknotes:
   for entry in s2:
     spl = entry.splitlines()
     if not any(re.search('language: (?!english)', s) for s in spl[1:]):
-      gid = spl[0].split()[-1].strip()
+      gid = int(spl[0].split()[-1].strip().replace('c', ''))
       gids.append(gid)
 
   if len(gids) > 0:
@@ -71,3 +83,7 @@ for book in sparknotes:
     bad += 1
 
 print good, bad, ugly
+
+with open(blacklist_path, 'w') as f:
+  for book in blacklist:
+    f.write('%s\n' % book)
