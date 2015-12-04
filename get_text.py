ecf959255307cc44d7333fe8396d615d0d791d7e
Tommy Zhang <trzhang@dhcp-18-189-87-202.dyn.mit.edu>
2015-12-03 23:04:49 -0500

    updated raw texts to not include foreign languages, plays, and poems

--- get_text.py
+++ get_text.py
@@ -1,17 +1,24 @@
 from gutenberg.acquire import load_etext
 from gutenberg.cleanup import strip_headers
+import os
 import string
 
 OUT_PATH = 'raw_texts/%s.txt'
 
-with open('gutenberg/good.txt') as f:
-  for line in f:
-    spl = line.split('|')
-    book = spl[0]
-    uid = int(spl[3].strip(string.lowercase + '\n'))
-    try:
-      text = strip_headers(load_etext(uid))
-      with open(OUT_PATH % book, 'w') as out:
-        out.write(text.encode('utf-8'))
-    except ValueError as e:
-      print '%s|%s' % (book, uid), e
+def process_file(filename):
+  with open(filename) as f:
+    for line in f:
+      spl = line.split('|')
+      book = spl[0]
+      uids = map(int, spl[3].strip(string.lowercase + '\n').split(','))
+      try:
+        with open(OUT_PATH % book, 'w') as out:
+          for uid in uids:
+            text = strip_headers(load_etext(uid))
+            out.write(text.encode('utf-8'))
+      except ValueError as e:
+        print '%s|%s' % (book, uid), e
+        os.remove(OUT_PATH % book)
+
+process_file('gutenberg/good.txt')
+process_file('gutenberg/ugly_fixed.txt')
