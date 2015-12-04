002f174b400825322f735f90b32f904c1f45ba66
Tommy Zhang <trzhang@31-34-30.wireless.csail.mit.edu>
2015-12-03 20:28:44 -0500

    crossref update

--- crossref.py
+++ crossref.py
@@ -3,7 +3,10 @@ import os
 import re
 import urllib2
 
-url = 'http://www.sparknotes.com/lit/%s/characters.html'
+sparknotes_url = 'http://www.sparknotes.com/lit/%s/characters.html'
+google_url = 'http://www.google.com/search?q=%s %s'
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'
+headers = {'User-Agent': user_agent}
 sparknotes = os.listdir('books')
 gutenberg = open('gutenberg/GUTINDEX.ALL.txt').read().lower()
 good_file = open('gutenberg/good.txt', 'w')
@@ -12,19 +15,47 @@ ugly_file = open('gutenberg/ugly.txt', 'w')
 
 good, bad, ugly = 0, 0, 0
 for book in sparknotes:
-  page = urllib2.urlopen(url % book)
+  # Sparknotes fail check
+  try:
+    page = urllib2.urlopen(sparknotes_url % book)
+  except urllib2.HTTPError:
+    continue
+  # Get title and author from sparknotes
   soup = BeautifulSoup(page.read())
   title = soup.findAll('h1', {'class': 'title padding-btm-0'})[0].text.encode('utf-8').lower()
   author = soup.findAll('h2', {'class': 'author'})[0].text.encode('utf-8').lower()
   if len(author) == 0:
     continue
   last_name = author.split(' ')[-1]
+
+  # Get type of literary work from google
+  url = (google_url % (title, author if author != 'anonymous' else '')).strip().replace(' ', '%20')
+  request = urllib2.Request(url, None, headers)
+  page = urllib2.urlopen(request)
+  soup = BeautifulSoup(page.read())
+  tag = soup.findAll('div', {'class': '_gdf'})
+  if len(tag) == 0:
+    continue
+  desc = soup.findAll('div', {'class': '_gdf'})[0].text.lower()
+  if 'play' in desc or 'poem' in desc:
+    continue
+
+  # Author in gutenberg index
   s1 = title in gutenberg
-  r = '^(?!audio: ).*%s, by.*%s.*\d+c?(?=\r\n(?!.*\[language: (?!english)))' % (title, last_name)
-  s2 = re.search(r, gutenberg, re.M)
-  if s1 and s2:
-    uid = s2.group(0).split(' ')[-1].strip()
-    s = '%s|%s|%s|%s' % (book, title, author, uid)
+  # Author and title in gutenberg index in same entry
+  r = '^(?!audio: ).*%s, by.*%s.*\d+c?\r\n?(?:.+\r\n?)*' % (title, last_name)
+  s2 = re.findall(r, gutenberg, re.M)
+  # Filter our non english entries
+  gids = []
+  for entry in s2:
+    spl = entry.splitlines()
+    if not any(re.search('language: (?!english)', s) for s in spl[1:]):
+      gid = spl[0].split()[-1].strip()
+      gids.append(gid)
+
+  if len(gids) > 0:
+    gid = min(gids)
+    s = '%s|%s|%s|%s' % (book, title, author, gid)
     print s
     good_file.write('%s\n' % s)
     good += 1
