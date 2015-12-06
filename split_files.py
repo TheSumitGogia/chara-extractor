72be68c79b90c23d5f49afa0949596f0cc21a632
Sumit Gogia <summit@mit.edu>
2015-12-05 20:52:38 -0500

    fixed issue with non-deterministic ordering of files when splitting

--- split_files.py
+++ split_files.py
@@ -18,9 +18,9 @@ assert nlp_filenames == text_filenames == tokens_filenames, '%s and %s directori
 num_texts = len(os.listdir(RAW_TEXT_DIR))
 for n in partition_ids:
   partition = range(num_texts / num_partitions * n, num_texts / num_partitions * (n + 1))
-  nlp_partition = map(os.listdir(RAW_NLP_DIR).__getitem__, partition)
-  text_partition = map(os.listdir(RAW_TEXT_DIR).__getitem__, partition)
-  tokens_partition = map(os.listdir(TOKENS_DIR).__getitem__, partition)
+  nlp_partition = map(sorted(os.listdir(RAW_NLP_DIR)).__getitem__, partition)
+  text_partition = map(sorted(os.listdir(RAW_TEXT_DIR)).__getitem__, partition)
+  tokens_partition = map(sorted(os.listdir(TOKENS_DIR)).__getitem__, partition)
 
   with ZipFile('partition%d.zip' % n, 'w') as zf:
     map(lambda f: zf.write(RAW_NLP_DIR + f), nlp_partition)
