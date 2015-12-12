df1a466f11b0d10d2cd3ceefd226203b40381a12
Sumit Gogia <summit@mit.edu>
2015-12-12 04:12:51 -0500

    added learning wrapper script functionality

--- learning.py
+++ learning.py
@@ -1,18 +1,53 @@
 import argparse
-from training import *
+from chara.training import *
 import scipy.io as sio
 
 ''' Character and relation classifier training.
 '''
 
-def handle_translate_command(args):
-    pass
+def handle_getdata_command(args):
+    tp = args.type
+    split = args.split
+    seed = args.seed
+    featdir = args.featdir
+    labeldir = args.labeldir
+    outdir = args.outdir
+    if tp == 'char':
+        train_char.FEATURES_DIR = featdir + '/characters'
+        train_char.LABELS_DIR = labeldir
+        train_books, test_books = train_common.generate_train_test(split, seed, featdir + '/characters')
+        train_char.get_and_save_data(train_books, outdir)
 
-def handle_datasplit_command(args):
-    pass
+    elif tp == 'rel':
+        train_pair.FEATURES_DIR = featdir + '/relations'
+        train_pair.LABELS_DIR = labeldir
+        train_books, test_books = train_common.generate_train_test(split, seed, featdir + '/relations')
+        train_pair.get_and_save_data(train_books, outdir)
 
 def handle_train_command(args):
-    pass
+    tp = args.type
+    model = args.model
+    bias = args.bias
+    degree = args.degree
+    kernel = args.kernel
+    filt = args.filter
+    outdir = args.outdir
+    datadir = args.datadir
+
+    if tp == 'char':
+        train_method = vars(train_char)['train_%s' % model]
+        train_char.class_weight = {1:float(bias), 0:1}
+        train_char.kernel = kernel
+        train_char.degree = degree
+        train_char.filters = eval(filt)
+        train_char.train_from_file_and_save(datadir, train_method, outdir)
+    elif tp == 'rel':
+        train_method = vars(train_pair)['train_%s' % model]
+        train_pair.class_weight = {1:float(bias), 0:1}
+        train_pair.kernel = kernel
+        train_pair.degree = degree
+        train_pair.filters = eval(filt)
+        train_pair.train_from_file_and_save(datadir, train_method, outdir)
 
 if __name__ == '__main__':
 
@@ -30,12 +65,13 @@ if __name__ == '__main__':
     '''
 
     # not in place yet because of some code inflexibility
-    '''
     # datasplit command argument parsing
-    split_parser = subparsers.add_parser('datasplit', help='split features, labels into train/test sets')
-    split_parser.add_argument('-d', '--datadir', default='data', help='directory to get data files from')
-    split_parser.add_argument('-o', '--outdir', default='data', help='directory to output split data files to')
-    '''
+    getdata_parser = subparsers.add_parser('getdata', help='get formatted train/test data for sklearn classifiers')
+    getdata_parser.add_argument('-t', '--type', default='char', choices=['char', 'rel'], help='whether data is for characters or relations')
+    getdata_parser.add_argument('-f', '--featdir', default='data/features', help='directory with character and relation features')
+    getdata_parser.add_argument('-s', '--split', default=0.7, help='ratio of training data for data split')
+    getdata.parser.add_argument('--seed', default=None, help='seed for random split')
+    getdata_parser.add_argument('-o', '--outdir', default='data/training/clfdata', help='directory to output split data files to')
 
     # train command argument parsing
     train_parser = subparsers.add_parser('train', help='train binary classifier on features and labels')
@@ -43,12 +79,10 @@ if __name__ == '__main__':
     train_parser.add_argument('-m', '--model', default='svm')
     train_parser.add_argument('-b', '--bias', default=2)
     train_parser.add_argument('--degree', default=2)
-    train_parser.add_argument('-r', '--ratio', default='0.7', help='train test split ratio')
+    train_parser.add_argument('-d', '--datadir', default='data/training/clfdata', help='directory with training data')
     train_parser.add_argument('-k', '--kernel', default='rbf')
-    train_parser.add_argument('-f', '--featdir', default='features', help='directory to get features from')
-    train_parser.add_argument('-l', '--labeldir', default='labels', help='directory to get labels from')
     train_parser.add_argument('-x', '--filter', default=[], help='feature filter, regex string list')
-    train_parser.add_argument('-o', '--outdir', default='classifiers', help='output directory for classifier parameters')
+    train_parser.add_argument('-o', '--outdir', default='data/classifiers/clf', help='output directory for classifier parameters')
 
     args = parser.parse_args()
     '''
