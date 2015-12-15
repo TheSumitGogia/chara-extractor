87dadaa6b0654cbed5f820ff3b11be707ec83d0d
mzhang94 <mzhang94@mit.edu>
2015-12-14 19:22:44 -0500

    fix train pair

--- evaluate.py
+++ evaluate.py
@@ -9,17 +9,27 @@ def handle_quant_command(args):
     clfdir = args.clfdir
     featdir = args.featdir
     labeldir = args.labeldir
+    baseline = args.baseline
+    filt = args.filter
 
     if tp == 'char':
         train_char.FEATURES_DIR = featdir + '/characters'
         train_char.LABELS_DIR = labeldir
         train_char.verbose = False
-        train_char.evaluate_clf_from_file(clfdir)
+        train_char.set_filters(filt)
+        if baseline:
+            train_char.evaluate_baseline()
+        else:
+            train_char.evaluate_clf_from_file(clfdir)
     if tp == 'rel':
         train_pair.FEATURES_DIR = featdir + '/relations'
         train_pair.LABELS_DIR = labeldir
         train_pair.verbose = False
-        train_pair.evaluate_clf_from_file(clfdir)
+        train_pair.set_filters(filt)
+        if baseline:
+            train_pair.evaluate_baseline()
+        else:
+            train_pair.evaluate_clf_from_file(clfdir)
 
 def handle_qual_command(args):
     tp = args.type
@@ -32,12 +42,20 @@ def handle_qual_command(args):
         train_char.FEATURES_DIR = featdir + '/characters'
         train_char.LABELS_DIR = labeldir
         train_char.verbose = True
-        train_char.evaluate_clf_from_file(clfdir, books)
+        train_char.set_filters(filt)
+        if baseline:
+            train_char.evaluate_baseline()
+        else:
+            train_char.evaluate_clf_from_file(clfdir, books)
     if tp == 'rel':
         train_pair.FEATURES_DIR = featdir + '/relations'
         train_pair.LABELS_DIR = labeldir
         train_pair.verbose = True
-        train_pair.evaluate_clf_from_file(clfdir, books)
+        train_pair.set_filters(filt)
+        if baseline:
+            train_pair.evaluate_baseline()
+        else:
+            train_pair.evaluate_clf_from_file(clfdir, books)
 
 if __name__ == '__main__':
 
@@ -51,6 +69,8 @@ if __name__ == '__main__':
     clf_parser.add_argument('-c', '--clfdir', default='data/classifiers/clfparams', help='directory with classifier params')
     clf_parser.add_argument('-f', '--featdir', default='data/features', help='directory with features')
     clf_parser.add_argument('-l', '--labeldir', default='data/labels', help='directory with labels')
+    clf_parser.add_argument('-bl', '--baseline', action='store_true', default=False, help='baseline')
+    clf_parser.add_argument('-x', '--filter', default='[]', help='feature filter, regex string list')
 
     # test subcommand argument parsing
     test_parser = subparsers.add_parser('qual', help='output found characters for input books')
@@ -59,6 +79,8 @@ if __name__ == '__main__':
     test_parser.add_argument('-c', '--clfdir', default='data/classifiers/clfparams', help='directory with classifier params')
     test_parser.add_argument('-f', '--featdir', default='data/features', help='file/folder with features')
     test_parser.add_argument('-l', '--labeldir', default='data/labels', help='directory with labels')
+    test_parser.add_argument('-bl', '--baseline', action='store_true', default=False, help='baseline')
+    test_parser.add_argument('-x', '--filter', default='[]', help='feature filter, regex string list')
 
     args = parser.parse_args()
     if args.command == 'quant':
